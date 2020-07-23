249184b5cf5647d8e57bf1ff9cfa0b16b29dda83
Akndju <davidd.akindoju@gmail.com>
2020-07-23 17:04:42 +0100

    face detector server

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors')
+const knex = require ('knex')
+
+const Register = require('./Controllers/Register.js');
+const SignIn = require('./Controllers/SignIn.js');
+const Profile = require('./Controllers/Profile.js');
+const Image = require('./Controllers/Image.js');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: '',
+        database: 'Face-Detector'
+    }
+});
+
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+
+app.get('/', (req, res) => {res.send(database.users)})
+app.post('/SignIn', (req, res) => {SignIn.handleSignIn(req, res, db, bcrypt)})
+app.post('/Register', (req, res) => {Register.handleRegister(req, res, db, bcrypt)})
+app.get('/Profile/:id', (req, res) => {Profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => {Image.handleImage(req, res, db)})
+app.post('/imageUrl', (req, res) => {Image.handleAPICall(req, res)})
+
+app.listen(3001, () => {
+    console.log("app is running on port 3001")
+})
