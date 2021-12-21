dbfd86d540dd786cfe91772dbcf0e0e376f8ba0d
akindoju <davidd.akindoju@gmail.com>
2021-12-21 23:41:34 +0100

    Fixed sql syntax and installed docker-compose

--- postgres/tables/login.sql
+++ postgres/tables/login.sql
@@ -1,9 +1,9 @@
-BEGIN TRANSACTION
+BEGIN TRANSACTION;
 
 CREATE TABLE login (
     id serial PRIMARY KEY,
-    hash VARCHAR(100) NOT NULL
-    email text UNIQUE NOT NULL,
+    hash varchar(100) NOT NULL,
+    email text UNIQUE NOT NULL
 );
 
 COMMIT;
